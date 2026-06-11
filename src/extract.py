0dc680c3562d3eb0f1f1c54f42ca543efe079725
Tonmoy10 <tonmoysaha1225@gmail.com>
2026-06-12 00:32:15 +0100

    feat: Implement dynamic pagination and request rate limitation for API ingestion.

--- src/extract.py
+++ src/extract.py
@@ -1,14 +1,17 @@
 from config import API_URL,API_ID,API_KEY
-import requests,json,pathlib
+import requests,json,pathlib,time
 
 creds = {
     "app_id":API_ID,
     "app_key":API_KEY
 }
+data=[]
+for page in range(1,6):
+    response = requests.get(API_URL+f"/jobs/gb/search/{page}",params=creds)
+    response = response.json()
+    data.extend(response["results"])
+    time.sleep(1)
 
-response = requests.get(API_URL+"/jobs/gb/search/1",params=creds)
-
-data = response.json()
 save_path = pathlib.Path(__file__).resolve().parent.parent / "data" / "raw_jobs.json"
 
 with open(save_path,"w") as jobs:
